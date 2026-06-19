28da3055e802781fe227d18a4f6d30f7997f107f
Ajay Thakare <143386761+ajay-thakare@users.noreply.github.com>
2026-06-19 19:35:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let btn = document.getElementById('enterBtn');
+let main = document.getElementById('main'); 
+
+btn.addEventListener('click', () => {
+	main.innerHTML = "<h1>Entered Metaverse</h1>";
+});
